a1d41ae6728bf036f69e3422024c8496f83b0527
Kartik <securealpha@gmail.com>
2017-03-08 19:27:34 +0530

    Login php with session

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+$username = $_POST['uname'];
+$password = $_POST['psw'];
+$_SESSION['userid'] = $username;
+
+$cluster = Cassandra::cluster()
+		->withContactPoints('192.168.43.194')
+		->withPort(9042)
+		->withCredentials("medicard", "medicard")
+		->build();
+
+$keyspace = 'test';
+
+$session = $cluster->connect($keyspace);
+
+$statement = new Cassandra\SimpleStatement ("SELECT email, patient_password from patient_master where email = '".$username."' and patient_password = '".$password."' ALLOW FILTERING");
+
+$future = $session->executeAsync($statement);
+
+$result = $future->get();
+
+foreach ($result as $row) {
+	header("Location: patient_master.php");
+	die();
+}
+?>
--- patient_master.php
+++ patient_master.php
@@ -1,5 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+	
+	<?php
+
+	$username = $_POST['uname'];
+	$password = $_POST['psw'];
+	$_SESSION['userid'] = $username;
+
+	$cluster = Cassandra::cluster()
+			->withContactPoints('192.168.43.194')
+			->withPort(9042)
+			->withCredentials("medicard", "medicard")
+			->build();
+
+	$keyspace = 'test';
+
+	$session = $cluster->connect($keyspace);
+
+	$statement = new Cassandra\SimpleStatement ("SELECT email, patient_password from patient_master where email = '".$username."' and patient_password = '".$password."' ALLOW FILTERING");
+
+	$future = $session->executeAsync($statement);
+
+	$result = $future->get();
+
+	foreach ($result as $row) {
+		header("Location: patient_master.php");
+		die();
+	}
+	?>
+	
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
