0bb1008a19f99d4c601c243f803ceef6b586c6f9
Kartik <securealpha@gmail.com>
2017-03-10 17:44:47 +0530

    Rudimentary reports
    done

--- download_report.php
+++ download_report.php
@@ -6,22 +6,14 @@
     $keyspace = 'test';
 
     $session = $cluster->connect($keyspace);
-    //$imagename=$_FILES["report_form"]["name"]; 
-	//$imagetmp=file_get_contents($_FILES['report_form']['tmp_name']);
-	//$content = bin2hex($imagetmp);
-	$statement = new Cassandra\SimpleStatement('select * from reports where r_id='.$r_id.' ALLOW FILTERING');
+    $statement = new Cassandra\SimpleStatement('select blobAsascii(file) as file, time from reports where r_id='.$r_id.' ALLOW FILTERING');
 	$future= $session->executeAsync($statement);
-	echo ('select * from reports where r_id='.$r_id.' ALLOW FILTERING');
 	
 	$result = $future->get();
-
-	foreach ($result as $row) 
-	{
-		echo ('<th>'.$row['description'] . '</th>' . '<th>' . '<a href="download_report.php?r_id='.$row['r_id'] . '">REPORT LINK</a></th>' .  '<th>' .  $row['time']  . '</th>');
-											
-	     //header("Content-length: ");
-         header("Content-type: application/pdf");
-         //header("Content-Disposition: attachment; filename=$file");
-         echo $row['file'];
-	 }
+    $f_content=base64_decode($result[0]['file']);
+    
+    header('Content-disposition: attachment; filename='.$result[0]['time'].'.pdf');
+    header('content-type: application/octet-stream');
+	
+	echo ($f_content);
  ?>
--- upload_report.php
+++ upload_report.php
@@ -3,6 +3,7 @@
     $username = $_SESSION['userid'];
     $p_id = $_POST['search_pid'];
     $d_id = $_POST['d_id'];
+    $desc_report=$_POST['desc_report'];
     
     include 'connectivity.php';
 
@@ -11,10 +12,9 @@
     $session = $cluster->connect($keyspace);
     $imagename=$_FILES["report_form"]["name"]; 
 	$imagetmp=file_get_contents($_FILES['report_form']['tmp_name']);
-	$content = bin2hex($imagetmp);
-	$statement = new Cassandra\SimpleStatement("insert into reports(r_id,d_id,p_id,file,time) VALUES (uuid(),".$d_id.",".$p_id.",asciiAsBlob('$content'),'".date("d-m-Y h:i:s")."')");
+	$content = base64_encode($imagetmp);
+	//echo $content;
+	$statement = new Cassandra\SimpleStatement("insert into reports(r_id,d_id,p_id,description,file,time) VALUES (uuid(),".$d_id.",".$p_id.",'". $desc_report ."',asciiAsBlob('$content'),'".date("d-m-Y h:i:s")."')");
 	$future= $session->executeAsync($statement);date("d-m-Y h:i:s");
-	echo ("insert into reports(r_id,d_id,p_id,file,time) VALUES (uuid(),".$d_id.",".$p_id.",asciiAsBlob('$content'),'".date("d-m-Y h:i:s")."')");
+	echo ("insert into reports(r_id,d_id,p_id,description,file,time) VALUES (uuid(),".$d_id.",".$p_id.",'". $desc_report ."',asciiAsBlob('$content'),'".date("d-m-Y h:i:s")."'");
  ?>
-
-
