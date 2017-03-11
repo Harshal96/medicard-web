fcda611b7e28b65ee74817502ef8e68c1aee969f
Ria2507 <riamaheshwari2507@gmail.com>
2017-03-11 19:27:07 +0530

    nm

--- connectivity.php
+++ connectivity.php
@@ -1,10 +1,6 @@
 <?php
 $cluster = Cassandra::cluster()
-<<<<<<< Updated upstream
-		->withContactPoints('192.168.43.194')
-=======
 		->withContactPoints('192.168.43.219')
->>>>>>> Stashed changes
 		->withPort(9042)
 		->withCredentials("ria", "medicard")
 		->build();
