8cf22fe691ff9177c87bbd0b1413c9f3c31d0e97
Ria2507 <riamaheshwari2507@gmail.com>
2017-03-12 01:17:49 +0530

    most api done

--- service/index.php
+++ service/index.php
@@ -112,7 +112,9 @@ $app->group('/api', function () use ($app) {
 	$stIDFetch =new Cassandra\SimpleStatement ("SELECT patient_id from patient_master where email = '".$patient_email."' ALLOW FILTERING");
 	$fIDFetch = $session->executeAsync($stIDFetch);
 	$rIDFetch = $fIDFetch->get();
+
 	$patient_id = $rIDFetch[0]['patient_id'];
+
 	$statement = new Cassandra\SimpleStatement ("SELECT * from prescriptions where patient_id =".$patient_id." ALLOW FILTERING");
 	$query="SELECT * from prescriptions where patient_id =".$patient_id." ALLOW FILTERING";
 	$future = $session->executeAsync($statement);
@@ -194,6 +196,40 @@ for($c=0; $c<$countRows; $c++){
 $r_statement = new Cassandra\SimpleStatement ("SELECT blobAsascii(file) as file, r_id,p_id,d_id,time,description from reports where p_id = ".$result[0]['patient_id']." ALLOW FILTERING");
 $r_future = $session->executeAsync($r_statement);
 $r_result = $r_future->get();
+
+$countRows=$r_result->count();
+
+	$json_resp=array(array());
+
+	$a = array(); // array of columns
+for($c=0; $c<$countRows; $c++){
+    $a[$c] = array(); // array of cells for column $c
+    for($r=0; $r<5; $r++){ //Change according to the number of keys needed in JSON
+        $a[$c][$r] = 0;
+    }
+}
+	//Don't forget the $ sign
+	for ($i = 0; $i < $countRows; $i++) 
+	{
+    		$a[$i][0]=$r_result[$i]['r_id'];
+    		$a[$i][1]=$r_result[$i]['p_id'];
+    		$a[$i][2]=$r_result[$i]['d_id'];
+    		$a[$i][3]=$r_result[$i]['file'];
+    		$a[$i][4]=$r_result[$i]['time'];
+    		$a[$i][5]=$r_result[$i]['description'];
+	}
+
+	$out = array_values($a);
+	json_encode($out);
+	
+	return $response->withJson([
+		'cn' => $countRows,
+		'reports' => $out
+		//$result[0]['prescriptions_id']
+		]);
+	});
+/*
+
 foreach ($r_result as $r_row) 
 {
 	//$CurlConnect = curl_init();
@@ -263,10 +299,127 @@ $time     =$r_row['time'];
 $app->get('/doctor/prescriptions', function ($request, $response, $args) use ($app) {
 	include '../connectivity.php';
 	$allGetVars = $request->getQueryParams();
-	$doctor_email = $allGetVars['email'];
-	
+
+	$diseases= $allGetVars['diseases'];
+	$doctor_id= $allGetVars['doctor_id'];
+	$dop = $allGetVars['dop'];
+	$medicines= $allGetVars['medicines'];
+	$patient_id = $allGetVars['patient_id'];
+	$fees_charged= $allGetVars['fees_charged'];
+	$symptoms= $allGetVars['symptoms'];
 	$keyspace = 'test';
 	$session = $cluster->connect($keyspace);  
 
-});});
+	 $addPresToDB = new Cassandra\SimpleStatement("INSERT INTO prescriptions (prescriptions_id, diseases, doctor_id, dop,medicines,patient_id,fees_charged,symptoms) VALUES (uuid(),'".$diseases."',".$doctor_id.",'".$dop."','".$medicines."',".$patient_id.",".$fees_charged.",'".$symptoms."')");
+
+        $futurePresDB    = $session->executeAsync($addPresToDB); 
+
+	return $response->withJson([
+			'message' => "success",
+        ]);
+});
+
+// view prescriptions
+    $app->get('/doctor/prescriptions', function ($request, $response, $args) use ($app) {
+	include '../connectivity.php';
+	$allGetVars = $request->getQueryParams();
+	$patient_email = $allGetVars['email'];
+	$keyspace = 'test';
+	$session = $cluster->connect($keyspace);
+	$stIDFetch =new Cassandra\SimpleStatement ("SELECT patient_id from patient_master where email = '".$patient_email."' ALLOW FILTERING");
+	$fIDFetch = $session->executeAsync($stIDFetch);
+	$rIDFetch = $fIDFetch->get();
+
+	$patient_id = $rIDFetch[0]['patient_id'];
+
+	$statement = new Cassandra\SimpleStatement ("SELECT * from prescriptions where patient_id =".$patient_id." ALLOW FILTERING");
+	$query="SELECT * from prescriptions where patient_id =".$patient_id." ALLOW FILTERING";
+	$future = $session->executeAsync($statement);
+	$result = $future->get();
+
+	$countRows=$result->count();
+
+
+	$json_resp=array(array());
+
+	$a = array(); // array of columns
+for($c=0; $c<$countRows; $c++){
+    $a[$c] = array(); // array of cells for column $c
+    for($r=0; $r<7; $r++){ //Change according to the number of keys needed in JSON
+        $a[$c][$r] = 0;
+    }
+}
+	//Don't forget the $ sign
+	for ($i = 0; $i < $countRows; $i++) 
+	{
+    		$a[$i][0]=$result[$i]['prescriptions_id'];
+    		$a[$i][1]=$result[$i]['patient_id'];
+    		$a[$i][2]=$result[$i]['doctor_id'];
+    		$a[$i][3]=$result[$i]['dop'];
+    		$a[$i][4]=$result[$i]['symptoms'];
+    		$a[$i][5]=$result[$i]['diseases'];
+    		$a[$i][6]=$result[$i]['medicines'];
+    		$a[$i][7]=$result[$i]['fees_charged'];
+	}
+
+	$out = array_values($a);
+	json_encode($out);
+	
+	return $response->withJson([
+		'prescriptions' => $out
+		//$result[0]['prescriptions_id']
+		]);
+
+	});
+
+	//view reports
+	$app->get('/doctors/reports', function ($request, $response, $args) use ($app) {
+	include '../connectivity.php';
+	$allGetVars = $request->getQueryParams();
+	$patient_email = $allGetVars['email'];
+	$keyspace = 'test';
+	$session = $cluster->connect($keyspace);
+
+	$statement = new Cassandra\SimpleStatement ("SELECT * from patient_master where email = '".$patient_email."' ALLOW FILTERING");
+	$future = $session->executeAsync($statement);
+	$result = $future->get();
+
+
+$r_statement = new Cassandra\SimpleStatement ("SELECT blobAsascii(file) as file, r_id,p_id,d_id,time,description from reports where p_id = ".$result[0]['patient_id']." ALLOW FILTERING");
+$r_future = $session->executeAsync($r_statement);
+$r_result = $r_future->get();
+
+$countRows=$r_result->count();
+
+	$json_resp=array(array());
+
+	$a = array(); // array of columns
+for($c=0; $c<$countRows; $c++){
+    $a[$c] = array(); // array of cells for column $c
+    for($r=0; $r<5; $r++){ //Change according to the number of keys needed in JSON
+        $a[$c][$r] = 0;
+    }
+}
+	//Don't forget the $ sign
+	for ($i = 0; $i < $countRows; $i++) 
+	{
+    		$a[$i][0]=$r_result[$i]['r_id'];
+    		$a[$i][1]=$r_result[$i]['p_id'];
+    		$a[$i][2]=$r_result[$i]['d_id'];
+    		$a[$i][3]=$r_result[$i]['file'];
+    		$a[$i][4]=$r_result[$i]['time'];
+    		$a[$i][5]=$r_result[$i]['description'];
+	}
+
+	$out = array_values($a);
+	json_encode($out);
+	
+	return $response->withJson([
+		'cn' => $countRows,
+		'reports' => $out
+		//$result[0]['prescriptions_id']
+		]);
+	});
+
+});
 $app->run();
