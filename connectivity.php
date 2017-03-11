f71108e3744ac4d2e7589f041708b5a14ded11d6
Mansi <mansi.dandiwala@gmail.com>
2017-03-11 16:06:07 +0530

    Patient profile shows reports table

--- connectivity.php
+++ connectivity.php
@@ -1,6 +1,6 @@
 <?php
 $cluster = Cassandra::cluster()
-		->withContactPoints('192.168.43.194')
+		->withContactPoints('192.168.43.38')
 		->withPort(9042)
 		->withCredentials("medicard", "medicard")
 		->build();
